ef2bd0e993bae8fe6486395bdc4981358f0c8430
ajaielango12 <113988029+ajaielango12@users.noreply.github.com>
2023-11-28 14:45:53 +0100

    Submission_Final_281123

--- src/pages/Changinggeographies.js
+++ src/pages/Changinggeographies.js
@@ -18,25 +18,25 @@ const Risingexports = () => {
       
       The traditional markets for Indian textile exports, such as the United States and Europe, are becoming increasingly competitive. 
         To diversify their export markets, Indian textile companies are expanding into new and emerging markets, including:
-        <strong>Africa: </strong>Africa has a large and growing population with a rising demand for textiles. 
+        <li></li><strong>Africa: </strong>Africa has a large and growing population with a rising demand for textiles. 
         Indian textile companies are setting up manufacturing units and establishing distribution networks in Africa to tap into this potential.
-        <strong>Latin America:</strong> Latin America is another promising market for Indian textile exports. The region has a strong demand for high-quality textiles, and Indian companies are well-positioned to meet this demand.
-        <strong>Asia: </strong>Asia is also a significant market for Indian textile exports. The region is home to a number of rapidly growing economies, and Indian companies are expanding their presence in these markets.
+        <li></li><strong>Latin America:</strong> Latin America is another promising market for Indian textile exports. The region has a strong demand for high-quality textiles, and Indian companies are well-positioned to meet this demand.
+        <li></li><strong>Asia: </strong>Asia is also a significant market for Indian textile exports. The region is home to a number of rapidly growing economies, and Indian companies are expanding their presence in these markets.
       
    </Text>
    <Text align="justify">
       
-    <strong>Establishing Global Presence:</strong>In addition to expanding into new markets, Indian textile companies are also taking steps to establish a stronger global presence. Some of the key strategies being used include:
-    <strong>Branding and Marketing:</strong> Indian textile companies are investing in branding and marketing campaigns to raise awareness of their products and services in international markets. 
+   <li></li> <strong>Establishing Global Presence:</strong>In addition to expanding into new markets, Indian textile companies are also taking steps to establish a stronger global presence. Some of the key strategies being used include:
+    <li></li><strong>Branding and Marketing:</strong> Indian textile companies are investing in branding and marketing campaigns to raise awareness of their products and services in international markets. 
     They are also participating in trade shows and exhibitions to showcase their latest products and technologies.
 
-    <strong>Design and Innovation:</strong> Indian textile companies are investing in design and innovation to create products that appeal to international consumers. 
+    <li></li><strong>Design and Innovation:</strong> Indian textile companies are investing in design and innovation to create products that appeal to international consumers. 
     They are also collaborating with international designers to develop new and unique products.
 
-    <strong>Supply Chain Management: </strong>Indian textile companies are improving their supply chain management practices to ensure that their products are delivered on time and at a competitive price. 
+    <li></li><strong>Supply Chain Management: </strong>Indian textile companies are improving their supply chain management practices to ensure that their products are delivered on time and at a competitive price. 
     They are also investing in technology to optimize their supply chains.
 
-    <strong>Partnerships and Collaborations:</strong>Indian textile companies are forming partnerships and collaborations with international companies to expand their reach and expertise. 
+    <li></li><strong>Partnerships and Collaborations:</strong>Indian textile companies are forming partnerships and collaborations with international companies to expand their reach and expertise. 
     These partnerships can help Indian companies gain access to new markets, technologies, and distribution channels.
 
 By diversifying their export markets and implementing these strategies, Indian textile companies are well-positioned to establish a strong global presence and continue to grow their export business.
--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,11 @@ const PageAbout = () => {
       <Heading>About</Heading>
       <Text>
         Hi, this is Ajai. This is about me. I am the designer of this page.{" "}
+        I am a Student at The CBS International Business School
+
+        All My Sources are derived
+        https://data.gov.in/search?title=textiles
+
       </Text>
     </Container>
   );
